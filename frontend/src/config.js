b71d3082f69021d9204daa2465f5f0ac528ae0d6
WenThing <wenthingteo2@gmail.com>
2026-01-06 00:34:25 +0800

    update config.js to use production API base URL

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000";  // or your actual Django backend URL
+// const API_BASE = "http://127.0.0.1:8000";  // or your actual Django backend URL
+const API_BASE = "https://fyp-designsensei.onrender.com";
 
 export default API_BASE;
